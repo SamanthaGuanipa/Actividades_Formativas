34d65c2b30f03b0d29ac7d70cbc2b6d50d03a66f
SamanthaGuanipa <70000311+SamanthaGuanipa@users.noreply.github.com>
2021-11-11 16:38:45 -0600

    Add files via upload

--- Act 1.2 - Algoritmos de Búsqueda y Ordenamiento/README.md
+++ Act 1.2 - Algoritmos de Búsqueda y Ordenamiento/README.md
@@ -1,35 +1,24 @@
-*ordenaSeleccion*
+*insertion*
 
-**Análisis de complejidad temporal**
+**Analisis de complejidad temporal**
 
-La complejidad del algoritmo es de O(n^2).
-Se compone de dos ciclos for (anidados) que recorren el arreglo para seleccionar el valor menor.  
-
-*ordenaBurbuja*
-
-**Análisis de complejidad temporal**
-
-La complejidad del algoritmo es de O(n^2).
-Se compone de dos ciclos for (anidados) que recorren el arreglo para seleccionar el valor mayor, llevándolo al final las veces necesarias.  
+El algoritmo recorrerá la linked list y agregará un nuevo elemento al final de la misma. 
+ Depende del tamaño de la lista, siendo complejidad O(n).
 
 
-*ordenaMerge*
+*search*
 
-**Análisis de complejidad temporal**
+**Analisis de complejidad temporal**
 
-La complejidad del algoritmo es de O(n^2).
-Es recursivo, consiste en dividir el arreglo a la mitad y lo va uniendo en orden.
+Se ingresa un valor, se recorrerá la lista hasta llegar a ese valor. Este proceso se realizará una sola vez. En caso de no estar el valor, se recorrerá la lista por completo, dependiendo del tamaño de la misma. La complejidad es de O(n)
 
-*busqSecuencial*
+#update
 
-**Análisis de complejidad temporal**
+**Analisis de complejidad temporal**
 
-La complejidad del algoritmo es O(n). 
-El número de pasos depende del tamaño del arreglo. Va comparando los valores del que se quiere encontrar con cada valor dado por el arreglo.  
+Se ingresa un valor, se recorrerá la lista hasta llegar a la posicion de ese valor, se actualiza el valor. La complejidad es de O(n). 
 
-*busquedaBinaria*
+#deleteAt
 
-**Análisis de complejidad temporal**
-
-La complejidad del algoritmo es O(log n).
-Al ir dividiendo a la mitad los arreglos, irá disminuyendo el número de pasos a realizar.
+**Analisis de complejidad temporal**
+Se ingresa un valor, se recorrerá la lista hasta llegar a la posicion de ese valor, se borra el elemento de esa posicion. La complejidad es de O(n). 
--- Act 1.2 - Algoritmos de Búsqueda y Ordenamiento/main.cpp
+++ Act 1.2 - Algoritmos de Búsqueda y Ordenamiento/main.cpp
@@ -4,90 +4,74 @@
  *  Created on: 10/08/2020
  *      Author: bvaldesa
  */
-
 #include <iostream>
-#include <string>
 #include <sstream>
-#include <vector>
 #include <cstring>
 #include <string>
-#include "sorts.h"
+#include "list.h"
 
 using namespace std;
 
-template <class T>
-string arrayToString(const vector<T> &v) {
-	stringstream aux;
 
-	aux << "[" << v[0];
-	for (int i = 1; i < v.size(); i++) {
-		aux << ", " << v[i];
+/* Agregar un metodo toString a su clase.h
+template <class T>
+std::string List<T>::toString() const {
+	std::stringstream aux;
+	Link<T> *p;
+	p = head;
+	aux << "[";
+	while (p != 0) {
+		aux << p->value;
+		if (p->next != 0) {
+			aux << ", ";
+		}
+		p = p->next;
 	}
 	aux << "]";
 	return aux.str();
 }
-
+*/
 
 int main(int argc, char* argv[]) {
+	List<int> b1;
+	string ans;
 
-	int array1[] = {58, 92, 75, 5, 21, 92, 84, 91, 64, 54, 3, 28, 11, 61, 65, 4, 1, 8, 52, 78};
-	vector<int> original1 (array1, array1 + sizeof(array1) / sizeof(int) );
+	b1.insertion(2);
+	ans = "[2]";
+	cout << "\n" <<"1.- esperada " << ans << "\n programa " << b1.toString() << "\n";
+	cout <<	(!ans.compare(b1.toString()) ? "success\n" : "fail\n");
 
-	int array2[] = {120, 10, 5, 897, 333, 68};
-	vector<int> original2 (array2, array2 + sizeof(array2) / sizeof(int) );
+	b1.insertion(3);
+	b1.insertion(6);
+	b1.insertion(10);
+	ans = "[2, 3, 6, 10]";
+	cout << "\n" <<"2.- esperada " << ans << "\n programa " << b1.toString() << "\n";
+	cout <<	(!ans.compare(b1.toString()) ? "success\n" : "fail\n");
 
-	vector<int> prueba;
-	Sorts<int> sorts;
-	string resp1 = "[1, 3, 4, 5, 8, 11, 21, 28, 52, 54, 58, 61, 64, 65, 75, 78, 84, 91, 92, 92]";
-	string resp2 = "[5, 10, 68, 120, 333, 897]";
+	cout << "\n" <<"3.- esperada " << 3 << " programa " << b1.search(10) << "\n";
+	cout <<	(3 == b1.search(10) ? "success\n" : "fail\n");
 
-	prueba = original1;
-	sorts.ordenaSeleccion(prueba);
+	cout << "\n" <<"4.- esperada " << -1 << " programa " << b1.search(1) << "\n";
+	cout <<	(-1 == b1.search(1) ? "success\n" : "fail\n");
 
+	b1.update(1,13);
+	ans = "[2, 13, 6, 10]";
+	cout << "\n" <<"5.- esperada " << ans << "\n programa " << b1.toString() << "\n";
+	cout <<	(!ans.compare(b1.toString()) ? "success\n" : "fail\n");
 
-	cout << "\n" <<"1.- esperada " << resp1 << "\n programa " << arrayToString(prueba) << "\n";
-	cout <<	(!resp1.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
+	b1.update(3,32);
+	ans ="[2, 13, 6, 32]";
+	cout << "\n" <<"6.- esperada " << ans << "\n programa " << b1.toString() << "\n";
+	cout << (!ans.compare(b1.toString()) ? "success\n" : "fail\n");
 
-	prueba = original2;
-	sorts.ordenaSeleccion(prueba);
+	b1.deleteAt(0);
+	ans = "[13, 6, 32]";
+	cout << "\n" <<"7.- esperada " << ans << "\n programa " << b1.toString() << "\n";
+	cout <<	(!ans.compare(b1.toString()) ? "success\n" : "fail\n");
 
-	cout << "\n" <<"2.- esperada " << resp2 << "\n programa " << arrayToString(prueba) << "\n";
-	cout <<	(!resp2.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
+	b1.deleteAt(1);
+	ans = "[13, 32]";
+	cout << "\n" <<"8.- esperada " << ans << "\n programa " << b1.toString() << "\n";
+	cout <<	(!ans.compare(b1.toString()) ? "success\n" : "fail\n");
 
-	prueba = original1;
-	sorts.ordenaBurbuja(prueba);
-
-	cout << "\n" <<"3.- esperada " << resp1 << "\n   programa " << arrayToString(prueba) << "\n";
-	cout <<	(!resp1.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
-
-	prueba = original2;
-	sorts.ordenaBurbuja(prueba);
-
-	cout << "\n" <<"4.- esperada "  << resp2 << "\n programa " << arrayToString(prueba) << "\n";
-	cout <<	(!resp2.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
-
-	prueba = original1;
-	sorts.ordenaMerge(prueba);
-
-	cout << "\n" <<"5.- esperada "  << resp1 << "\n programa " << arrayToString(prueba) << "\n";
-	cout <<	(!resp1.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
-
-	prueba = original2;
-	sorts.ordenaMerge(prueba);
-	cout << "\n" <<"6.- esperada "  << resp2 << "\n programa " << arrayToString(prueba) << "\n";
-	cout <<	(!resp2.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
-
-	sorts.ordenaMerge(prueba);
-
-	cout << "\n" <<"7.- esperada "  << 5 << " programa " << sorts.busqSecuencial(prueba, 897) << "\n";
-  cout <<	(5 == sorts.busqSecuencial(prueba, 897) ? "success\n" : "fail\n");
-
-	cout << "\n" <<"8.- esperada "  << -1  << " programa " << sorts.busqSecuencial(prueba, 100) << "\n";
-	cout <<	(-1 == sorts.busqSecuencial(prueba, 100) ? "success\n" : "fail\n");
-
-	cout << "\n" <<"9.- esperada "  << 4 << " programa " << sorts.busqBinaria(prueba, 333)  << "\n";
-	cout <<	(4 == sorts.busqBinaria(prueba, 333) ? "success\n" : "fail\n");
-
-	cout << "\n" <<"10.- esperada "  << -1  << " programa " << sorts.busqBinaria(prueba, 197) << "\n";
-	cout <<	(-1 == sorts.busqBinaria(prueba, 197) ? "success\n" : "fail\n");
 }
